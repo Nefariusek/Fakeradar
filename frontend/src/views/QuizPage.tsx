ca70b41b33f68ad423c3f76603dac3bb11a75833
Sylwia <mangoweb@wp.pl>
2022-06-04 21:51:39 +0200

    get random fake and true statement

--- frontend/src/views/QuizPage.tsx
+++ frontend/src/views/QuizPage.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
@@ -7,36 +7,50 @@ import Typography from '@mui/material/Typography';
 import { fakeStatements } from '../data/fakeStatements';
 import { trueStatements } from '../data/trueStatements';
 import { QUIZ_TITLE, QUIZ_SUBTITLE } from '../constants/strings';
+import randomIntFromInterval from '../services/randomIntFromInterval';
 
-const QuizPage: React.FunctionComponent = (): ReactElement => (
-  <Container maxWidth="md">
-    <Box textAlign="center" color="primary.main">
-      <Typography variant="h1" color="info.main" fontWeight="550">
-        {QUIZ_TITLE}
-      </Typography>
-      <Typography variant="h5" marginTop="7%" color="primary.main">
-        {QUIZ_SUBTITLE}
-      </Typography>
-    </Box>
+const QuizPage: React.FunctionComponent = (): ReactElement => {
+  const [fakeStatement, setFakeStatement] = useState('');
+  const [trueStatement, setTrueStatement] = useState('');
 
-    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 4 }} m={4}>
-      <Grid item xs={6}>
-        <Typography component="p" align="center">
-          {fakeStatements[5].text}
+  useEffect(() => {
+    const fakeStatementNumber = randomIntFromInterval(1, fakeStatements.length);
+    const trueStatementNumber = randomIntFromInterval(1, trueStatements.length);
+
+    setFakeStatement(fakeStatements[fakeStatementNumber - 1].text);
+    setTrueStatement(trueStatements[trueStatementNumber - 1].text);
+  }, []);
+
+  return (
+    <Container maxWidth="md">
+      <Box textAlign="center" color="primary.main">
+        <Typography variant="h1" color="info.main" fontWeight="550">
+          {QUIZ_TITLE}
         </Typography>
-      </Grid>
-      <Grid item xs={6}>
-        <Typography component="p" align="center">
-          {trueStatements[7].text}
+        <Typography variant="h5" marginTop="7%" color="primary.main">
+          {QUIZ_SUBTITLE}
         </Typography>
+      </Box>
+
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 4 }} m={4}>
+        <Grid item xs={6}>
+          <Typography component="p" align="center">
+            {fakeStatement}
+          </Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography component="p" align="center">
+            {trueStatement}
+          </Typography>
+        </Grid>
+        <Grid item container justifyContent="center" xs={12} m={2}>
+          <Button variant="contained" color="primary">
+            Play again
+          </Button>
+        </Grid>
       </Grid>
-      <Grid item container justifyContent="center" xs={12} m={2}>
-        <Button variant="contained" color="primary">
-          Play again
-        </Button>
-      </Grid>
-    </Grid>
-  </Container>
-);
+    </Container>
+  );
+};
 
 export default QuizPage;
