07c0d36844419145ec5b68e2554cb5ff5dba2924
NataliaCichonska <natalia.cichonska1103@gmail.com>
2022-06-04 14:58:22 +0200

    change destination

--- frontend/src/constants/paths.ts
+++ frontend/src/constants/paths.ts
@@ -3,6 +3,3 @@ export const PATH_TO_SPONSORPAGE = '/sponsors';
 export const PATH_TO_REPORTPAGE = '/reports';
 export const PATH_TO_EDUCATIONPAGE = '/education';
 export const PATH_TO_QUIZPAGE = '/quiz';
-
-export const PATH_TO_LIVECHAT_WEBSITE =
-  'https://www.livechat.com/pricing/?a=1PSQl6aWg&utm_campaign=pp_trial&utm_source=PP&gclid=Cj0KCQjwheyUBhD-ARIsAHJNM-OnjLph-n9wP59qm-M1jJ32sHg8dnBocI9ZSEG7l9AkazY9sKTYHqAaAhsqEALw_wcB';
--- frontend/src/views/HomePage.tsx
+++ frontend/src/views/HomePage.tsx
@@ -3,30 +3,25 @@ import { Box } from '@mui/system';
 import React, { ReactElement } from 'react';
 import { Link } from 'react-router-dom';
 import { divStyle } from '../constants/pagesStyles';
-import {
-  PATH_TO_EDUCATIONPAGE,
-  PATH_TO_QUIZPAGE,
-  PATH_TO_REPORTPAGE,
-  PATH_TO_LIVECHAT_WEBSITE,
-} from '../constants/paths';
+import { PATH_TO_EDUCATIONPAGE, PATH_TO_QUIZPAGE, PATH_TO_REPORTPAGE, PATH_TO_SPONSORPAGE } from '../constants/paths';
 import { QUOTE, QUOTE_AUTHOR, TITLE } from '../constants/strings';
 
 const HomePage: React.FunctionComponent = (): ReactElement => {
   const isCaptchaOk = true;
   return (
     <div className="homePage" style={divStyle}>
-      <a href={PATH_TO_LIVECHAT_WEBSITE} target="_blank">
-        <Button
-          size="large"
-          style={{
-            position: 'absolute',
-            top: '10px',
-            right: '10px',
-          }}
-        >
-          SPONSORS
-        </Button>
-      </a>
+      <Button
+        component={Link}
+        to={PATH_TO_SPONSORPAGE}
+        size="large"
+        style={{
+          position: 'absolute',
+          top: '10px',
+          right: '10px',
+        }}
+      >
+        SPONSORS
+      </Button>
       <Grid container direction="column" justifyContent="space-around" alignItems="center" min-height="100%">
         <Box textAlign="center" color="primary.main">
           <Typography variant="h1" color="info.main" fontWeight="550">
