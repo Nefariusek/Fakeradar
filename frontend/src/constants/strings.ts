d7000256084f11f21327131a0b9095ac677549f2
Urszuja <urszula.jarych@gmail.com>
2022-06-04 21:53:16 +0200

    add form submission

--- frontend/src/constants/strings.ts
+++ frontend/src/constants/strings.ts
@@ -1,3 +1,6 @@
 export const TITLE = 'FakeRADAR';
 export const QUOTE = '"Beware of false knowledge, it is more dangerous than ignorance"';
 export const QUOTE_AUTHOR = 'George Bernard Shaw';
+export const CONFIRMATION_TEXT = 'Report the troll?';
+export const REPORT_TROLL = 'Kill it with fire!';
+export const LEAVE_HIM_ALONE = 'Leave him aloooone!';
--- frontend/src/views/FormPage.tsx
+++ frontend/src/views/FormPage.tsx
@@ -1,7 +1,9 @@
-import { Button, Checkbox, FormControlLabel, Grid, MenuItem, TextField, Typography } from '@mui/material';
-import React, { ReactElement, useState } from 'react';
+import { Box, Button, Checkbox, FormControlLabel, Grid, MenuItem, TextField, Typography } from '@mui/material';
+import React, { FormEvent, ReactElement, useState } from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
 //@ts-ignore
 import FileUpload from 'react-mui-fileuploader';
+import FormConfirmation from '../components/FormConfirmation';
 import { divStyle } from '../constants/pagesStyles';
 import { TITLE } from '../constants/strings';
 
@@ -41,6 +43,8 @@ const FormPage: React.FunctionComponent = (): ReactElement => {
   const [name, setName] = useState('');
   const [contact, setContact] = useState('');
   const [isChecked, setIsChecked] = useState(false);
+  const [verified, setVerified] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
 
   const handleChange = (event: any) => {
     setSource(event.target.value);
@@ -58,10 +62,12 @@ const FormPage: React.FunctionComponent = (): ReactElement => {
     }
   };
 
-  const handleFormSubmission = () => {
-    if (validateForm()) {
-      console.log('sent');
-    }
+  const handleValidation = () => {
+    // if (validateForm()) {
+    //   console.log('sent');
+
+    // }
+    setIsClicked(!isClicked);
   };
 
   const handleFileUploadError = (error: any) => {
@@ -72,124 +78,148 @@ const FormPage: React.FunctionComponent = (): ReactElement => {
     // Do something...
   };
 
+  function onReCaptchaChange(recaptchaValue: string | null) {
+    console.log('Captcha clicked');
+    setVerified(true);
+  }
+
   return (
     <div className="FormPage" style={divStyle}>
-      <Grid container textAlign="center" direction="row" justifyContent="center" gap="50px" alignItems="center">
-        <img width="150px" height="150px" src="./radar_logo.svg" alt="logo"></img>
-        <Typography variant="h1" color="info.main" fontWeight="550">
-          {TITLE}
-        </Typography>
-      </Grid>
-      <Grid container alignItems="center" justifyContent="space-around" direction="column" gap="2px" marginTop="40px">
-        <TextField
-          id="standard-select-currency"
-          select
-          value={source}
-          label="Source of phishing:"
-          onChange={handleChange}
-          helperText="Please select source of phishing"
-          required
-          variant="filled"
-          style={textFieldStyle}
-        >
-          {sources.map((src) => (
-            <MenuItem key={src.value} value={src.value}>
-              {src.label}
-            </MenuItem>
-          ))}
-        </TextField>
-        <TextField
-          helperText="Paste the link from a message"
-          required
-          label="Massage:"
-          variant="filled"
-          style={textFieldStyle}
-        ></TextField>
-        {source === 'sms' && (
-          <TextField
-            helperText="Enter senders phone number"
-            required
-            label="Senders phone number:"
-            variant="filled"
-            style={textFieldStyle}
-          ></TextField>
-        )}
-        {source === 'mail' && (
-          <TextField
-            helperText="Enter senders address"
-            required
-            label="Senders address:"
-            variant="filled"
-            style={textFieldStyle}
-          ></TextField>
-        )}
-        {source === 'social media' && (
-          <TextField
-            helperText="Enter senders username"
-            required
-            label="Senders username:"
-            variant="filled"
-            style={textFieldStyle}
-          ></TextField>
-        )}
-        <TextField
-          id="outlined-multiline-static"
-          variant="outlined"
-          label="Your problem:"
-          helperText="Describe Your problem"
-          multiline
-          rows="8"
-          style={textFieldStyle}
-        ></TextField>
-        <FileUpload
-          multiFile={true}
-          disabled={false}
-          title="Please upload a screenshot of the message"
-          header="Drag and drop file"
-          leftLabel="or"
-          rightLabel="to select files"
-          buttonLabel="click here"
-          buttonRemoveLabel="Remove all"
-          maxFileSize={10}
-          maxUploadFiles={3}
-          maxFilesContainerHeight={357}
-          maxFilesContainerWidth={700}
-          minFilesContainerWidth={700}
-          errorSizeMessage={'An error occured'}
-          allowedExtensions={['jpg', 'jpeg', 'png']}
-          onFilesChange={handleFilesChange}
-          onError={handleFileUploadError}
-          bannerProps={{ elevation: 0, variant: 'outlined' }}
-          containerProps={{ elevation: 0, variant: 'outlined' }}
-        />
-        <FormControlLabel
-          value="clicked"
-          control={<Checkbox onChange={handleCheckboxChange} />}
-          label="Did You click the link?"
-          labelPlacement="end"
-        />
-        {isChecked && (
-          <Grid container alignItems="center" justifyContent="center" direction="row" gap="60px">
-            <TextField
-              helperText="Please enter Your name"
-              required
-              label="Name:"
-              variant="filled"
-              style={smallTextFieldStyle}
-            ></TextField>
-            <TextField
-              helperText="Leave contact"
-              required
-              label="Contact:"
-              variant="filled"
-              style={smallTextFieldStyle}
-            ></TextField>
+      {!verified ? (
+        <>
+          <Grid container textAlign="center" direction="row" justifyContent="center" gap="50px" alignItems="center">
+            <img width="150px" height="150px" src="./radar_logo.svg" alt="logo"></img>
+            <Typography variant="h1" color="info.main" fontWeight="550">
+              {TITLE}
+            </Typography>
           </Grid>
-        )}
-        <Button variant="contained" onClick={handleFormSubmission}>
-          Submit
-        </Button>
-      </Grid>
+          <Grid
+            container
+            alignItems="center"
+            justifyContent="space-around"
+            direction="column"
+            gap="2px"
+            marginTop="40px"
+          >
+            <TextField
+              id="standard-select-currency"
+              select
+              value={source}
+              label="Source of phishing:"
+              onChange={handleChange}
+              helperText="Please select source of phishing"
+              required
+              variant="filled"
+              style={textFieldStyle}
+            >
+              {sources.map((src) => (
+                <MenuItem key={src.value} value={src.value}>
+                  {src.label}
+                </MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              helperText="Paste the link from a message"
+              required
+              label="Massage:"
+              variant="filled"
+              style={textFieldStyle}
+            ></TextField>
+            {source === 'sms' && (
+              <TextField
+                helperText="Enter senders phone number"
+                required
+                label="Senders phone number:"
+                variant="filled"
+                style={textFieldStyle}
+              ></TextField>
+            )}
+            {source === 'mail' && (
+              <TextField
+                helperText="Enter senders address"
+                required
+                label="Senders address:"
+                variant="filled"
+                style={textFieldStyle}
+              ></TextField>
+            )}
+            {source === 'social media' && (
+              <TextField
+                helperText="Enter senders username"
+                required
+                label="Senders username:"
+                variant="filled"
+                style={textFieldStyle}
+              ></TextField>
+            )}
+            <TextField
+              id="outlined-multiline-static"
+              variant="outlined"
+              label="Your problem:"
+              helperText="Describe Your problem"
+              multiline
+              rows="8"
+              style={textFieldStyle}
+            ></TextField>
+            <FileUpload
+              multiFile={true}
+              disabled={false}
+              title="Please upload a screenshot of the message"
+              header="Drag and drop file"
+              leftLabel="or"
+              rightLabel="to select files"
+              buttonLabel="click here"
+              buttonRemoveLabel="Remove all"
+              maxFileSize={10}
+              maxUploadFiles={3}
+              maxFilesContainerHeight={357}
+              maxFilesContainerWidth={700}
+              minFilesContainerWidth={700}
+              errorSizeMessage={'An error occured'}
+              allowedExtensions={['jpg', 'jpeg', 'png']}
+              onFilesChange={handleFilesChange}
+              onError={handleFileUploadError}
+              bannerProps={{ elevation: 0, variant: 'outlined' }}
+              containerProps={{ elevation: 0, variant: 'outlined' }}
+            />
+            <FormControlLabel
+              value="clicked"
+              control={<Checkbox onChange={handleCheckboxChange} />}
+              label="Did You click the link?"
+              labelPlacement="end"
+            />
+            {isChecked && (
+              <Grid container alignItems="center" justifyContent="center" direction="row" gap="60px">
+                <TextField
+                  helperText="Please enter Your name"
+                  required
+                  label="Name:"
+                  variant="filled"
+                  style={smallTextFieldStyle}
+                ></TextField>
+                <TextField
+                  helperText="Leave contact"
+                  required
+                  label="Contact:"
+                  variant="filled"
+                  style={smallTextFieldStyle}
+                ></TextField>
+              </Grid>
+            )}
+            {!isClicked ? (
+              <Button variant="contained" onClick={handleValidation}>
+                Submit
+              </Button>
+            ) : (
+              <Grid item sx={{ minHeight: '90px' }}>
+                <ReCAPTCHA sitekey={import.meta.env.VITE_RECAPTCHA_SITE_KEY} onChange={onReCaptchaChange} />
+              </Grid>
+            )}
+          </Grid>
+        </>
+      ) : (
+        <FormConfirmation />
+      )}
     </div>
   );
 };
