b253565e793f86b8b6148f0ee6c9ba786338e919
NataliaCichonska <natalia.cichonska1103@gmail.com>
2022-06-04 23:04:20 +0200

    resolve conflict

--- frontend/src/views/FormPage.tsx
+++ frontend/src/views/FormPage.tsx
@@ -117,129 +117,6 @@ const FormPage: React.FunctionComponent = (): ReactElement => {
   }
 
   return (
-<<<<<<< HEAD
-    <>
-      <NavTabs view="REPORT" />
-      <div className="FormPage" style={divStyle}>
-        <Grid container textAlign="center" direction="row" justifyContent="center" gap="50px" alignItems="center">
-          <img width="150px" height="150px" src="./radar_logo.svg" alt="logo"></img>
-          <Typography variant="h1" color="info.main" fontWeight="550">
-            {TITLE}
-          </Typography>
-        </Grid>
-        <Grid container alignItems="center" justifyContent="space-around" direction="column" gap="2px" marginTop="40px">
-          <TextField
-            id="standard-select-currency"
-            select
-            value={source}
-            label="Source of phishing:"
-            onChange={handleChange}
-            helperText="Please select source of phishing"
-            required
-            variant="filled"
-            style={textFieldStyle}
-          >
-            {sources.map((src) => (
-              <MenuItem key={src.value} value={src.value}>
-                {src.label}
-              </MenuItem>
-            ))}
-          </TextField>
-          <TextField
-            helperText="Paste the link from a message"
-            required
-            label="Massage:"
-            variant="filled"
-            style={textFieldStyle}
-          ></TextField>
-          {source === 'sms' && (
-            <TextField
-              helperText="Enter senders phone number"
-              required
-              label="Senders phone number:"
-              variant="filled"
-              style={textFieldStyle}
-            ></TextField>
-          )}
-          {source === 'mail' && (
-            <TextField
-              helperText="Enter senders address"
-              required
-              label="Senders address:"
-              variant="filled"
-              style={textFieldStyle}
-            ></TextField>
-          )}
-          {source === 'social media' && (
-            <TextField
-              helperText="Enter senders username"
-              required
-              label="Senders username:"
-              variant="filled"
-              style={textFieldStyle}
-            ></TextField>
-          )}
-          <TextField
-            id="outlined-multiline-static"
-            variant="outlined"
-            label="Your problem:"
-            helperText="Describe Your problem"
-            multiline
-            rows="8"
-            style={textFieldStyle}
-          ></TextField>
-          <FileUpload
-            multiFile={true}
-            disabled={false}
-            title="Please upload a screenshot of the message"
-            header="Drag and drop file"
-            leftLabel="or"
-            rightLabel="to select files"
-            buttonLabel="click here"
-            buttonRemoveLabel="Remove all"
-            maxFileSize={10}
-            maxUploadFiles={3}
-            maxFilesContainerHeight={357}
-            maxFilesContainerWidth={700}
-            minFilesContainerWidth={700}
-            errorSizeMessage={'An error occured'}
-            allowedExtensions={['jpg', 'jpeg', 'png']}
-            onFilesChange={handleFilesChange}
-            onError={handleFileUploadError}
-            bannerProps={{ elevation: 0, variant: 'outlined' }}
-            containerProps={{ elevation: 0, variant: 'outlined' }}
-          />
-          <FormControlLabel
-            value="clicked"
-            control={<Checkbox onChange={handleCheckboxChange} />}
-            label="Did You click the link?"
-            labelPlacement="end"
-          />
-          {isChecked && (
-            <Grid container alignItems="center" justifyContent="center" direction="row" gap="60px">
-              <TextField
-                helperText="Please enter Your name"
-                required
-                label="Name:"
-                variant="filled"
-                style={smallTextFieldStyle}
-              ></TextField>
-              <TextField
-                helperText="Leave contact"
-                required
-                label="Contact:"
-                variant="filled"
-                style={smallTextFieldStyle}
-              ></TextField>
-            </Grid>
-          )}
-          <Button variant="contained" onClick={handleFormSubmission}>
-            Submit
-          </Button>
-        </Grid>
-      </div>
-    </>
-=======
     <div className="FormPage" style={divStyle}>
       {!verified ? (
         <>
@@ -384,7 +261,6 @@ const FormPage: React.FunctionComponent = (): ReactElement => {
         <FormConfirmation />
       )}
     </div>
->>>>>>> develop
   );
 };
 export default FormPage;
--- frontend/src/views/Sponsorspage.tsx
+++ frontend/src/views/Sponsorspage.tsx
@@ -1,52 +1,58 @@
 import { Box, Button, Grid, Typography } from '@mui/material';
 import React, { ReactElement } from 'react';
 import { Link } from 'react-router-dom';
+import NavTabs from '../components/NavTabs';
 import { CODERSCAMP_LOGO, LIVECHAT_LOGO } from '../constants/logosUrls';
 import { sponsorsDivStyle } from '../constants/pagesStyles';
 import { PATH_TO_HOMEPAGE } from '../constants/paths';
 import { CODERSCAMP_URL, LIVECHAT_URL } from '../constants/sponsorsUrls';
 import { SPONSORSPAGE_TITLE } from '../constants/strings';
-import NavTabs from '../components/NavTabs';
 
 const Sponsorspage: React.FunctionComponent = (): ReactElement => (
   <>
-  <NavTabs />
-  <div style={sponsorsDivStyle}>
-    <Button
-      component={Link}
-      to={PATH_TO_HOMEPAGE}
-      size="large"
-      style={{
-        position: 'absolute',
-        top: '10px',
-        right: '10px',
-      }}
-    >
-      BACK TO RADAR
-    </Button>
-    <Grid container direction="column" justifyContent="space-around" alignItems="center" min-height="100%" width="100%">
-      <Box textAlign="center" color="primary.main">
-        <img src="./radar_logo.svg" alt="logo"></img>
-        <Typography variant="h2" marginTop="7%" color="primary.main" marginBottom="10%">
-          {SPONSORSPAGE_TITLE}
-        </Typography>
-      </Box>
-      <a href={CODERSCAMP_URL} target="_blank">
-        <img src={CODERSCAMP_LOGO} alt="LiveChat" />
-      </a>
-      <a href={LIVECHAT_URL} target="_blank">
-        <img
-          src={LIVECHAT_LOGO}
-          alt=".CodersCamp"
-          style={{
-            marginTop: '1%',
-          }}
-        />
-      </a>
-    </Grid>
-  </div>
+    <NavTabs view="" />
+    <div style={sponsorsDivStyle}>
+      <Button
+        component={Link}
+        to={PATH_TO_HOMEPAGE}
+        size="large"
+        style={{
+          position: 'absolute',
+          top: '10px',
+          right: '10px',
+        }}
+      >
+        BACK TO RADAR
+      </Button>
+      <Grid
+        container
+        direction="column"
+        justifyContent="space-around"
+        alignItems="center"
+        min-height="100%"
+        width="100%"
+      >
+        <Box textAlign="center" color="primary.main">
+          <img src="./radar_logo.svg" alt="logo"></img>
+          <Typography variant="h2" marginTop="7%" color="primary.main" marginBottom="10%">
+            {SPONSORSPAGE_TITLE}
+          </Typography>
+        </Box>
+        <a href={CODERSCAMP_URL} target="_blank">
+          <img src={CODERSCAMP_LOGO} alt="LiveChat" />
+        </a>
+        <a href={LIVECHAT_URL} target="_blank">
+          <img
+            src={LIVECHAT_LOGO}
+            alt=".CodersCamp"
+            style={{
+              marginTop: '1%',
+            }}
+          />
+        </a>
+      </Grid>
+    </div>
   </>
-  
 );
 
 export default Sponsorspage;
