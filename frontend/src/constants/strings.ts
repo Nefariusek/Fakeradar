a8837c342bdb145cf7412b2a35529265909dcf02
GRosza <grzegorz.roszak@coderscrew.pl>
2022-06-05 02:17:27 +0200

    minor change

--- frontend/src/constants/strings.ts
+++ frontend/src/constants/strings.ts
@@ -3,7 +3,7 @@ export const QUOTE = '"Beware of false knowledge, it is more dangerous than igno
 export const QUOTE_AUTHOR = 'George Bernard Shaw';
 export const SPONSORSPAGE_TITLE = 'Meet our sponsor:';
 
-const PHISHING1 = `1.Legit companies don’t request your sensitive information via email.  \nChances are if you receive an unsolicited email from an institution that provides a link or attachment and asks you to provide sensitive information, it’s a scam. Most companies will not send you an email asking for passwords, credit card information, credit scores, or tax numbers, nor will they send you a link from which you need to login.`;
+const PHISHING1 = `1. Legit companies don’t request your sensitive information via email.  \nChances are if you receive an unsolicited email from an institution that provides a link or attachment and asks you to provide sensitive information, it’s a scam. Most companies will not send you an email asking for passwords, credit card information, credit scores, or tax numbers, nor will they send you a link from which you need to login.`;
 const PHISHING2 =
   '2. Legit companies usually call you by your name. Phishing emails typically use generic salutations such as “Dear valued member,” “Dear account holder,” or “Dear customer.” If a company you deal with required information about your account, the email would call you by name and probably direct you to contact them via phone, some hackers simply avoid the salutation altogether. This is especially common with advertisements.';
 const PHISHING3 =
